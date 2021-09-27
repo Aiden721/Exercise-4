ab4bfdf9a8f3f1ab48748da4b7d0d19f6c97b3c7
Aiden Sterling <90715623+Aiden721@users.noreply.github.com>
2021-09-27 17:45:56 +0000

    got rid of "import math"

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 # Date: 9/27/21
 # Description: Creating an input function that gives the average of three integers.
 
-# Import Math
-import math
-
 # The Input
 number_1 = int(input("Enter an integer: "))
 number_2 = int(input("Enter another integer: "))
