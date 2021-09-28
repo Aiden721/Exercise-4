7f6dc1766bd3a38e8a9522843a4b2d885df4bb00
Aiden Sterling <90715623+Aiden721@users.noreply.github.com>
2021-09-28 16:17:08 +0000

    re fixed it

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 # Description: Creating an input function that gives the average of three integers.
 
 # The Input
-number_1 = int(input("Enter an integer: "))
-number_2 = int(input("Enter another integer: "))
-number_3 = int(input("Enter one more integer: "))
+int_one = int(input("Enter an integer: "))
+int_two = int(input("Enter another integer: "))
+int_three = int(input("Enter one more integer: "))
 print()
 
 # The Output
-print(f"The average is: {(number_1 + number_2 + number_3)/3}.")
+print(f"The average is: {(int_one + int_two + int_three)/3}.")
